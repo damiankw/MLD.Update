f3bdae8fe7f96fa7555845d05dedd2f3b23c9364
damian <dwest@gazman.com.au>
2018-08-28 12:01:38 +1000

    Added first container, what are we doing here?

---
+++ MajorLeague.php
@@ -0,0 +1,43 @@
+<?php
+
+// download the data
+$HTML = file_get_contents('http://www.majorleaguedodgeball.com.au/footscray-mixed-league-wednesdays/');
+echo $HTML;
+
+// convert into readable data
+
+// check the rounds
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
